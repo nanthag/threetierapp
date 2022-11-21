d0d0794b43924b2c72c5ad7b8cb1845b12b260d5
nanthags <nanthags@gmail.com>
2022-11-21 16:02:11 +0530

    Add project files.

---
+++ threetierapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace threetierapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
